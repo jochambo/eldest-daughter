daa8da7d6fab56aaa19e5b1828ca4b0dec8dcacc
jochambo <joey.chamberlin@gmail.com>
2014-12-24 12:17:46 -0500

    Updated README. Removed jQuery references

--- README.md
+++ README.md
@@ -3,11 +3,22 @@ eldest-daughter
 
 A Front-end Design Challenge for Eldest Daughter
 
-** Getting started
-_All dependencies are included in the repo under the vendor profile._
-  _- Modernizr.JS: For improved cross-browser support_
-_Further optimization can be made by minimizing all html, css and js files._
-1. Clone repo locally.
+## Getting started
+1. Clone this repo locally.
 2. Fire off local server using 
-```python -m SimpleHTTPServer 8000```
-3. Navigate to localhost:8000
+`python -m SimpleHTTPServer 8000`
+3. Navigate to `localhost:8000`
+
+_All dependencies (only one at this point) are included in the repo under the vendor profile._
+- Modernizr.JS: For improved cross-browser support
+
+_Further optimization can be had by minimizing all html, css and js files._
+
+### Logging In
+Obviously this is just a front-end challenge, so I only added basic HTML5 validations using `required` and Javascript validations alerting whether username and password are correct.
+
+**Username** = `username`/
+**Password** = `password`
+
+### Mobile
+I developed this using [localtunnel](https://localtunnel.me) with my iPhone5 and an iPhone4s (the only things I had readily on hand). Beyond that I used the latest versions of all major browsers and used fallbacks and vendor prefixes where needed. Modernizr should assist back to IE8.
--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,23 @@
-require.config({
-  paths: {
-    jquery: '../bower_components/jquery/jquery'
-  },
-  shim: {
-    bootstrap: {
-      deps: ['jquery'],
-      exports: 'jquery'
-    }
-  }
-});
-
-require(['app', 'jquery'], function (app, $) {
+(function(){
   'use strict';
+  
   if (!('autofocus' in document.createElement('input'))) { // Add focus if browser doesn't support
     document.getElementById('username').focus();
   }
-});
+  
+  function validateForm() {
+    var un = document.loginform.user.value,
+        pw = document.loginform.pword.value,
+        username = 'username', // Used in lieu of full auth system.
+        password = 'password';
 
-function validateForm() {
-  var un = document.loginform.user.value,
-      pw = document.loginform.pword.value,
-      username = 'username', // Used in lieu of full auth system.
-      password = 'password';
-
-  if ((un === username) && (pw === password)) {
-    alert('Welcome ' + username + '!');
-    return true;
-  } else {
-    alert('Login was unsuccessful, please check your username and password and try again.');
-    return false;
+    if ((un === username) && (pw === password)) {
+      alert('Welcome ' + username + '!');
+      return true;
+    } else {
+      alert('Login was unsuccessful, please check your username and password and try again.');
+      return false;
+    }
   }
-}
+
+}();)
